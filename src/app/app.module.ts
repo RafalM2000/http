cd0ac9502ad9ccbd184f13a40d459b45c0e80553
Rafal <rmf@europe.com>
2020-04-24 06:39:06 +0200

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { OneComponent } from './one/one.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appRouter: Routes = [
   {path: '', component: OneComponent},
@@ -18,7 +22,10 @@ export const appRouter: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRouter)
+    HttpClientModule,
+    MatListModule,
+    RouterModule.forRoot(appRouter),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
