5f387c645686745d043543fa3161f2f8b156b69d
Rafal <rmf@europe.com>
2020-05-01 06:34:41 +0200

    Revert "undo changes in examp2"
    This reverts commit 34ce1eacbbb9e9cf398d3cb802296ea17caa8dac.

--- src/app/two/two.component.ts
+++ src/app/two/two.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -9,11 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class TwoComponent implements OnInit {
 
   subTitle: string = 'Fetching text from a file';
+  exampleText: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
 
+    this.http.get('/data/authors.json', {responseType: 'text'})
+    .subscribe(
+      res => this.exampleText = res,
+      err => console.log('Błąd pobierania pliku: ', err)
+    );
   }
 
 }
